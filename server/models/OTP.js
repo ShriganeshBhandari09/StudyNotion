c4a4418b72018c9f01e9db740f4ce687d0eeb7ea
Shriganesh Bhandari <shriganesh.bhandari693@gmail.com>
2024-01-09 15:43:11 +0530

    Added OTP function for email verification

--- server/models/OTP.js
+++ server/models/OTP.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const mailSender = require("../utils/mailSender");
 
 const OTPSchema = new mongoose.Schema({
   email: {
@@ -15,6 +16,23 @@ const OTPSchema = new mongoose.Schema({
     expires: 5*60
   },
 });
+
+// a function to send emails
+
+async function sendVerification(email, otp){
+  try{
+    const mailResponse = await mailSender(email, "Verification Email from StudyNotion", otp);
+    console.log("Email Sent Successfully", mailResponse)
+  }catch(error){
+    console.log("Error occured while sending mail", error);
+    throw error;
+  }
+}
+
+OTPSchema.pre("save", async function(next){
+  await sendVerificationEmail(this.email, this.otp);
+  next();
+})
 
 module.exports = mongoose.model("OTP", OTPSchema);
  
