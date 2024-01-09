7abc6bf7b42e2e8590c5950b096e79612a3b843c
Shriganesh Bhandari <shriganesh.bhandari693@gmail.com>
2024-01-09 15:18:11 +0530

    Connected DB and created models

---
+++ server/models/Course.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const courseSchema = new mongoose.Schema({
+  courseName: {
+    type: String,
+    required: true,
+  },
+  courseDescription: {
+    type: String,
+    required: true,
+  },
+  instructor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  whatWillYouLearn: {
+    type: String,
+  },
+  courseContent: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Section",
+    },
+  ],
+  ratingAndReview: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "RatingAndReview",
+    },
+  ],
+  price:{
+    type: Number,
+  },
+  thumbnail:{
+    type:String,
+    required: true,
+  },
+  tag:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref:"Tag"
+  },
+  studentsEnrolled:[{
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref:"User",
+  }],
+});
+
+module.exports = mongoose.model("Course", courseSchema);
